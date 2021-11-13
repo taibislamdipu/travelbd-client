935f504fd7c7b4d1dd9da6abcdf5b528f5ee7e5e
taibislamdipu <mailtaibislam@gmail.com>
2021-11-13 22:09:02 +0600

    doc update

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div className="container text-center py-5">
-            <h2>404! page not found!</h2>
+            <div className="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
+                <div className="py-5">
+                    <p>404 error! </p>
+                    <h2 className="display-4 fw-bold">This page d💔esn't seem to exist.</h2>
+                    <Link to={'/'}>
+                        <button className="btn custom-black-btn mt-5">Back to Home</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
